5ab9e9cae2ba4b6926461c91a4dcbe19a50a2c69
bingcheng1998 <bingcheng1998@qq.com>
2022-04-07 19:03:45 -0400

    failed 音素法 1

--- code/init.sh
+++ code/init.sh
@@ -4,3 +4,5 @@ source activate /home/bh2283/penv/
 du -hs ./*
 du -hs .[^.]*
 myquota
+
+singularity exec --overlay zh-audio.ext3 /scratch/work/public/singularity/cuda11.3.0-cudnn8-devel-ubuntu20.04.sif /bin/bash
